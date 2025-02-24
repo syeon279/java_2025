aabd410ead7d9f336937a63634256f0d33716f19
syeon279 <syeonblue@gmail.com>
2025-02-24 18:14:31 +0900

    HTML + SCC + JS

--- java_basic/src/com/company/java006_ex/ArrayEx005.java
+++ java_basic/src/com/company/java006_ex/ArrayEx005.java
@@ -1,5 +1,7 @@
 package com.company.java006_ex;
 
+import java.util.Arrays;
+
 public class ArrayEx005 {
 
 	public static void main(String[] args) {
@@ -10,16 +12,16 @@ public class ArrayEx005 {
 			if(ch[i]>='a' && ch[i]<='z') {
 				countSmall++;
 				char s2 = ch[i];
-				System.out.println((char)(s2-32));
+				System.out.print((char)(s2-32));
 			} else if(ch[i]>='A' && ch[i]<='Z') {
 				countBig++;
 				char b2 = ch[i];
-				System.out.println((char)(b2+32));
+				System.out.print((char)(b2+32));
 			}
 
 	}
-		System.out.println("소문자의 수: "+countSmall);
+		System.out.println("\n소문자의 수: "+countSmall);
 		System.out.println("대문자의 수: "+countBig);
-
+		System.out.println(Arrays.toString(ch));
 	}
 }
